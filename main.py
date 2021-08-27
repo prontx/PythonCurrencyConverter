415fd0495007a59a78751f9db5962faabc90de0e
Matsvei Hauryliuk <79801523+prontx@users.noreply.github.com>
2021-08-27 21:44:59 +0200

    Added the first row

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+import tkinter as tk
 
 def convert():
     print('Enter the source currency: ')
@@ -28,5 +29,34 @@ def convert():
     result = json.loads(result)
     print(result["result"])
 
+def graphics():
+    window = tk.Tk()
+
+    window.rowconfigure(0, minsize=50, weight=1)
+    window.columnconfigure([0, 1, 2, 3], minsize=50, weight=1)
+
+    frame_amount = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=1)
+    frame_amount.grid(row=0, column=0, padx=10, pady=10)
+    label_amount = tk.Label(master=frame_amount, text='Amount')
+    label_amount.pack(padx=10, pady=10)
+
+    frame_from = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=1)
+    frame_from.grid(row=0, column=1, padx=10, pady=10)
+    label_from = tk.Label(master=frame_from, text='From')
+    label_from.pack(padx=10, pady=10)
+
+    frame_swap = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=1)
+    frame_swap.grid(row=0, column=2, padx=10, pady=10)
+    label_swap = tk.Label(master=frame_swap,text='Swap')
+    label_swap.pack(padx=10, pady=10)
+
+    frame_to = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=1)
+    frame_to.grid(row=0, column=3, padx=10, pady=10)
+    label_to = tk.Label(master=frame_to,text='To')
+    label_to.pack(padx=10, pady=10)
+
+    window.mainloop()
+
 if __name__ == "__main__":
-    convert()
+    #convert()
+    graphics()
