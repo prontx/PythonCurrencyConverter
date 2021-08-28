3fa8cf917c718025fa7c32606342ae26f94db657
Matsvei Hauryliuk <79801523+prontx@users.noreply.github.com>
2021-08-28 11:46:32 +0200

    Improved the look a bit(fonts, paddings, backgrounds)

--- main.py
+++ main.py
@@ -33,39 +33,40 @@ def graphics():
     window = tk.Tk()
 
     window.title('Currency Converter App')
+    window.configure(background='#ec6627')
 
     window.rowconfigure([0, 1], minsize=50, weight=1)
-    window.columnconfigure([0, 1, 2, 3], minsize=50, weight=1)
+    window.columnconfigure([0, 1, 2, 5], minsize=50, weight=1)
 
     # Beginning of row 1
 
-    frame_amount = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=1)
-    frame_amount.grid(row=0, column=0, padx=10, pady=10)
-    label_amount = tk.Label(master=frame_amount, text='Amount')
-    label_amount.pack(padx=10, pady=10)
+    frame_amount = tk.Frame(master=window, relief=tk.FLAT, borderwidth=1, background='#ec6627')
+    frame_amount.grid(row=0, column=0, padx=5, pady=5, sticky='w')
+    label_amount = tk.Label(master=frame_amount, text='Amount', font='Helvetica 14 bold', background='#ec6627')
+    label_amount.pack(padx=5, pady=5)
 
-    frame_from = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=1)
-    frame_from.grid(row=0, column=1, padx=10, pady=10)
-    label_from = tk.Label(master=frame_from, text='From')
-    label_from.pack(padx=10, pady=10)
+    frame_from = tk.Frame(master=window, relief=tk.FLAT, borderwidth=1, background='#ec6627')
+    frame_from.grid(row=0, column=1, padx=5, pady=5, sticky='w')
+    label_from = tk.Label(master=frame_from, text='From', font='Helvetica 14 bold', background='#ec6627')
+    label_from.pack(padx=5, pady=5)
 
-    frame_swap = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=1)
-    frame_swap.grid(row=0, column=2, padx=10, pady=10)
-    label_swap = tk.Label(master=frame_swap,text='Swap')
-    label_swap.pack(padx=10, pady=10)
+    frame_swap = tk.Frame(master=window, relief=tk.FLAT, borderwidth=1, background='#ec6627')
+    frame_swap.grid(row=0, column=2, padx=5, pady=5)
+    label_swap = tk.Label(master=frame_swap,text='Swap', font='Helvetica 14 bold', background='#ec6627')
+    label_swap.pack(padx=5, pady=5)
 
-    frame_to = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=1)
-    frame_to.grid(row=0, column=3, padx=10, pady=10)
-    label_to = tk.Label(master=frame_to,text='To')
-    label_to.pack(padx=10, pady=10)
+    frame_to = tk.Frame(master=window, relief=tk.FLAT, borderwidth=1, background='#ec6627')
+    frame_to.grid(row=0, column=5, padx=5, pady=5, sticky='w')
+    label_to = tk.Label(master=frame_to,text='To', font='Helvetica 14 bold', background='#ec6627')
+    label_to.pack(padx=5, pady=5)
 
     # End of row 1
     # Beginning of row 2
 
-    frame_amount_entry = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=1)
-    frame_amount_entry.grid(row=1, column=0, padx=10, pady=10)
+    frame_amount_entry = tk.Frame(master=window, relief=tk.FLAT, borderwidth=1)
+    frame_amount_entry.grid(row=1, column=0, padx=5, pady=5)
     label_amount_entry = tk.Entry(master=frame_amount_entry)
-    label_amount_entry.pack(padx=10, pady=10)
+    label_amount_entry.pack(padx=5, pady=5)
 
     OPTIONS = [
         "USD",
@@ -73,20 +74,20 @@ def graphics():
         "CZK"
     ]
 
-    frame_from_choose = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=1)
-    frame_from_choose.grid(row=1, column=1, padx=10, pady=10)
+    frame_from_choose = tk.Frame(master=window, relief=tk.FLAT, borderwidth=1)
+    frame_from_choose.grid(row=1, column=1, padx=5, pady=5)
     from_entry_variable = tk.StringVar(window)
     from_entry_variable.set(OPTIONS[0])
     w = tk.OptionMenu(frame_from_choose, from_entry_variable, *OPTIONS)
     w.pack()
 
-    frame_swap_button = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=1)
-    frame_swap_button.grid(row=1, column=2, padx=10, pady=10)
+    frame_swap_button = tk.Frame(master=window, relief=tk.FLAT, borderwidth=1)
+    frame_swap_button.grid(row=1, column=2, padx=5, pady=5)
     swap_button = tk.Button(master=frame_swap_button, text="-->\n<--")
-    swap_button.pack(padx=10, pady=10)
+    swap_button.pack(padx=5, pady=5)
 
-    frame_convert_button = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=1)
-    frame_convert_button.grid(row=1, column=3, padx=10, pady=10)
+    frame_convert_button = tk.Frame(master=window, relief=tk.FLAT, borderwidth=1)
+    frame_convert_button.grid(row=1, column=5, padx=5, pady=5)
     convert_button = tk.Button(master=frame_convert_button, text="Convert")
     convert_button.pack(padx=5, pady=5)
 
