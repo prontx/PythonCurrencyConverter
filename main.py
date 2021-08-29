28b66e04c32066e58bd7f01043271cfed91cf12a
Matsvei Hauryliuk <79801523+prontx@users.noreply.github.com>
2021-08-29 22:31:56 +0200

    Implemented convert and swap, also supporting keyboard input

--- main.py
+++ main.py
@@ -10,32 +10,15 @@ import tkinter as tk
 
 # Global variables
 
-frame_amount         = None
-label_amount         = None
-frame_from           = None 
-label_from           = None
-frame_swap           = None
-label_swap           = None
-frame_to             = None
-label_to             = None
-frame_amount_entry   = None
-label_amount_entry   = None
-frame_from_choose    = None
-from_entry_variable  = None
-w                    = None
-frame_swap_button    = None
-swap_button          = None
-frame_to_choose      = None
-to_entry_variable    = None
-frame_convert_button = None
-convert_button       = None
-cur_from             = None
-amount_from          = None
-cur_to               = None
-window               = None
+frame_amount = label_amount = frame_from = label_from = None
+frame_swap = label_swap = frame_to = label_to = None
+frame_amount_entry = label_amount_entry = frame_from_choose = from_entry_variable  = None
+w = frame_swap_button = swap_button = frame_to_choose = None
+to_entry_variable = frame_convert_button = convert_button = cur_from = None
+amount_from = cur_to = window = None
 
 def convert(cur_from, amount_from, cur_to):
-    #global window
+    global window
     url = "https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert"
     querystring = {"from":f"{cur_from}","to":f"{cur_to}","amount":f"{amount_from}"}
     load_dotenv()
@@ -47,7 +30,14 @@ def convert(cur_from, amount_from, cur_to):
     response = requests.request("GET", url, headers=headers, params=querystring)
     result = response.text
     result = json.loads(result)
-    print(result["result"])
+    result = result["result"]
+    
+    frame_result = tk.Frame(master=window, relief=tk.FLAT, borderwidth=1, background='#ec6627')
+    frame_result.grid(row=2, column=0, columnspan=2, padx=5, pady=5, sticky='nsew')
+    label_result = tk.Label(master=frame_result, text=f'''The result of conversion of 
+        {amount_from} {cur_from} to {cur_to} is: \n{result} {cur_to}''', 
+        font='Helvetica 14 bold', background='#ec6627')
+    label_result.pack(padx=5, pady=5)
 
 def graphics():
     global window
@@ -59,25 +49,12 @@ def graphics():
     window.rowconfigure([0, 1, 2], minsize=50, weight=1)
     window.columnconfigure([0, 1, 2, 3], minsize=50, weight=1)
 
-    global frame_amount
-    global label_amount
-    global frame_from 
-    global label_from
-    global frame_swap
-    global label_swap
-    global frame_to
-    global label_to
-    global frame_amount_entry  
-    global label_amount_entry   
-    global frame_from_choose
-    global from_entry_variable  
-    global w
-    global frame_swap_button    
-    global swap_button
-    global frame_to_choose
-    global to_entry_variable
-    global frame_convert_button 
-    global convert_button
+    global frame_amount; global label_amount; global frame_from; global label_from
+    global frame_swap; global label_swap; global frame_to; global label_to
+    global frame_amount_entry; global label_amount_entry; global frame_from_choose
+    global from_entry_variable; global w; global frame_swap_button    
+    global swap_button; global frame_to_choose; global to_entry_variable
+    global frame_convert_button; global convert_button
 
     # Beginning of row 1
 
@@ -124,7 +101,7 @@ def graphics():
 
     frame_swap_button = tk.Frame(master=window, relief=tk.FLAT, borderwidth=1, background='#ec6627')
     frame_swap_button.grid(row=1, column=2, padx=5, pady=5)
-    swap_button = tk.Button(master=frame_swap_button, text="-->\n<--")
+    swap_button = tk.Button(master=frame_swap_button, text="-->\n<--", command=swap_currencies)
     swap_button.pack(padx=5, pady=5)
 
     frame_to_choose = tk.Frame(master=window, relief=tk.FLAT, borderwidth=1, background='#ec6627')
@@ -134,13 +111,12 @@ def graphics():
     w = tk.OptionMenu(frame_to_choose, to_entry_variable, *OPTIONS)
     w.pack()
 
-
     # End of row 2
     # Beginning of row 3
 
     frame_convert_button = tk.Frame(master=window, relief=tk.FLAT, borderwidth=1, background='#ec6627')
     frame_convert_button.grid(row=2, column=3, padx=5, pady=5)
-    convert_button = tk.Button(master=frame_convert_button, text="Convert", command=handle_inputs)
+    convert_button = tk.Button(master=frame_convert_button, text="Convert", command = handle_inputs)
     convert_button.pack(padx=5, pady=5)
 
     # End of row 3
@@ -154,8 +130,41 @@ def handle_inputs():
     amount_from = label_amount_entry.get()
     cur_from = from_entry_variable.get()
     cur_to = to_entry_variable.get() 
-    convert(cur_from, amount_from, cur_to)   
+    convert(cur_from, amount_from, cur_to)
+
+def handle_inputs(event):
+    global cur_from
+    global cur_to
+    global amount_from
+
+    amount_from = label_amount_entry.get()
+    cur_from = from_entry_variable.get()
+    cur_to = to_entry_variable.get() 
+    convert(cur_from, amount_from, cur_to)
+    print(event.char)
+
+# Handles swap button pressed
+def swap_currencies():
+    global from_entry_variable
+    global to_entry_variable
+    auxiliary_var1 = from_entry_variable.get()
+    auxiliary_var2 = to_entry_variable.get()
+    from_entry_variable.set(auxiliary_var2)
+    to_entry_variable.set(auxiliary_var1)
+
+# Handles S key pressed
+def swap_currencies(event):
+    global from_entry_variable
+    global to_entry_variable
+    auxiliary_var1 = from_entry_variable.get()
+    auxiliary_var2 = to_entry_variable.get()
+    from_entry_variable.set(auxiliary_var2)
+    to_entry_variable.set(auxiliary_var1)
+    print(event.char)
 
 if __name__ == "__main__":
     graphics()
+    # Binding the Enter key to show output
+    window.bind('<Return>', handle_inputs)
+    window.bind('S', lambda event: swap_currencies(event))
     window.mainloop()
